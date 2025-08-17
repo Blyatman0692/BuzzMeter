e209afb87fed031adafba20635d19a49d6b12a18
Blyatman0692 <jzheng2@seattleu.edu>
2025-08-17 12:47:52 -0700

    Initial Commit

---
+++ BuzzMeter/BuzzMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuzzMeterApp.swift
+//  BuzzMeter
+//
+//  Created by Junwen Zheng on 8/17/25.
+//
+
+import SwiftUI
+
+@main
+struct BuzzMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
